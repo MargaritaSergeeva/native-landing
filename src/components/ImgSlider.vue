6cf57cc19d63b808c6a59cb93cadf2935e259403
Margarita Sergeeva <margaritk@inbox.ru>
2022-12-11 01:01:14 +0300

    added first section to main page

---
+++ src/components/ImgSlider.vue
@@ -0,0 +1,41 @@
+<template>
+  <swiper
+    class="img-slider"
+    :modules="[EffectFade, Autoplay]"
+    effect="fade"
+    :fade-effect="{ crossFade: true }"
+    :speed="1500"
+    :autoplay="{
+              delay: 4500,
+              disableOnInteraction: false,
+            }"
+    :allow-touch-move="false"
+    loop
+  >
+    <swiper-slide class="img-slider__slide">
+      <picture>
+        <img class="img-slider__img img-slider__img--phone" src="~assets/main/phone1.png" srcset="~assets/main/phone1.png 1x, ~assets/main/phone1-2x.png 2x" alt="phone">
+      </picture>
+    </swiper-slide>
+    <swiper-slide class="img-slider__slide">
+      <picture>
+        <img class="img-slider__img img-slider__img--laptop" src="~assets/main/laptop1.png" srcset="~assets/main/laptop1.png 1x, ~assets/main/laptop1-2x.png 2x" alt="laptop">
+      </picture>
+    </swiper-slide>
+    <swiper-slide class="img-slider__slide">
+      <picture>
+        <img class="img-slider__img img-slider__img--phone" src="~assets/main/phone2.png" srcset="~assets/main/phone2.png 1x, ~assets/main/phone2-2x.png 2x" alt="phone">
+      </picture>
+    </swiper-slide>
+    <swiper-slide class="img-slider__slide">
+      <picture>
+        <img class="img-slider__img img-slider__img--laptop" src="~assets/main/laptop2.png" srcset="~assets/main/laptop2.png 1x, ~assets/main/laptop2-2x.png 2x" alt="laptop">
+      </picture>
+    </swiper-slide>
+  </swiper>
+</template>
+
+<script setup lang="ts">
+import { EffectFade, Autoplay } from 'swiper';
+import { Swiper, SwiperSlide } from 'swiper/vue';
+</script>
---
+++ src/pages/MainPage.vue
@@ -0,0 +1,26 @@
+<template>
+  <q-page class="main-page">
+    <section class="main-page__main-section main-section ">
+      <div class="main-section__container container">
+        <div class="main-section__text-block">
+          <h1><span class="custom-font main-section__title">NativеLab –</span> платформа нативной рекламы.</h1>
+          <p class="main-section__text">Более <b>6 лет</b> опыта на международном рекламном рынке!</p>
+          <q-btn class="main-btn main-btn--blue main-btn--desktop">получить презентацию и кейсы</q-btn>
+        </div>
+        <div class="main-section__img-block">
+          <ImgSlider/>
+        </div>
+        <q-btn class="main-btn main-btn--blue main-btn--mob">получить презентацию и кейсы</q-btn>
+      </div>
+    </section>
+    <section class="geo container">
+      <h2>Широкий охват ГЕО</h2>
+      <p>Мы предлагаем рекламодателям качественный нативный трафик из более чем 22 стран мира</p>
+
+    </section>
+  </q-page>
+</template>
+
+<script setup lang="ts">
+import ImgSlider from 'components/ImgSlider.vue'
+</script>
