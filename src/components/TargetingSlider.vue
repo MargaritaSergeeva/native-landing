b923c7078e5e4e4ff33ff7695ec1c71ec44c0de3
Margarita Sergeeva <margaritk@inbox.ru>
2022-12-11 21:12:49 +0300

    added targeting section

---
+++ src/components/TargetingSlider.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="targeting-slider">
+    <swiper
+      class="targeting-slider__slider"
+      :modules="[EffectFade]"
+      effect="fade"
+      :fade-effect="{ crossFade: true }"
+      :speed="1500"
+      :allow-touch-move="true"
+      @swiper="setSwiperRef"
+    >
+      <swiper-slide class="targeting-slider__slide">
+        <picture>
+          <img class="targeting-slider__img" src="~assets/main/begining-article.png" srcset="~assets/main/begining-article.png 1x, ~assets/main/begining-article-2x.png 2x" alt="begining article">
+        </picture>
+      </swiper-slide>
+      <swiper-slide class="targeting-slider__slide">
+        <picture>
+          <img class="targeting-slider__img" src="~assets/main/ending-article.png" srcset="~assets/main/ending-article.png 1x, ~assets/main/ending-article-2x.png 2x" alt="ending article">
+        </picture>
+      </swiper-slide>
+      <swiper-slide class="targeting-slider__slide">
+        <picture>
+          <img class="targeting-slider__img" src="~assets/main/middle-article.png" srcset="~assets/main/middle-article.png 1x, ~assets/main/middle-article-2x.png 2x" alt="middle article">
+        </picture>
+      </swiper-slide>
+      <swiper-slide class="targeting-slider__slide">
+        <picture>
+          <img class="targeting-slider__img" src="~assets/main/side-article.png" srcset="~assets/main/side-article.png 1x, ~assets/main/side-article-2x.png 2x" alt="side article">
+        </picture>
+      </swiper-slide>
+    </swiper>
+    <p class="targeting-slider__append-buttons">
+      <button :class="{ 'active': slideNumber === 1 }" @click="slideTo(1)">В начале статьи</button>
+      <button :class="{ 'active': slideNumber === 2 }" @click="slideTo(2)">В конце статьи</button>
+      <button :class="{ 'active': slideNumber === 3 }" @click="slideTo(3)">В середине статьи</button>
+      <button :class="{ 'active': slideNumber === 4 }" @click="slideTo(4)">На правой боковой панели</button>
+    </p>
+  </div>
+
+</template>
+
+<script setup>
+import { ref } from 'vue';
+import { EffectFade } from 'swiper';
+import { Swiper, SwiperSlide } from 'swiper/vue';
+
+let swiperRef = null;
+const slideNumber = ref(1);
+
+const setSwiperRef = (swiper) => {
+  swiperRef = swiper;
+};
+
+const slideTo = (index) => {
+  swiperRef.slideTo(index - 1, 1500);
+  slideNumber.value = index;
+};
+</script>
--- src/pages/MainPage.vue
+++ src/pages/MainPage.vue
@@ -21,8 +21,80 @@
       </div>
     </section>
     <section class="targeting-section">
-      <h2 class="targeting-section__title title title--white custom-font">Форматы и таргетинги</h2>
-      <p class="targeting-section__intro intro intro--white">TGB • TGB In-Read • Баннер</p>
+      <div class="targeting-section__container container">
+        <h2 class="targeting-section__title title title--white custom-font">Форматы и таргетинги</h2>
+        <p class="targeting-section__intro intro intro--white">TGB • TGB In-Read • Баннер</p>
+        <div class="targeting-section__desktop">
+            <TargetingSlider/>
+        </div>
+        <div class="targeting-section__mob">
+          <q-list class="targeting-section__mob-list">
+            <q-item class="targeting-section__mob-item">
+              <picture>
+                <img class="targeting-section__mob-img" src="~assets/main/begining-article.png" srcset="~assets/main/begining-article.png 1x, ~assets/main/begining-article-2x.png 2x" alt="begining article">
+              </picture>
+              <p class="targeting-section__mob-text">В начале статьи</p>
+            </q-item>
+            <q-item class="targeting-section__mob-item">
+              <picture>
+                <img class="targeting-section__mob-img" src="~assets/main/ending-article.png" srcset="~assets/main/ending-article.png 1x, ~assets/main/ending-article-2x.png 2x" alt="ending article">
+              </picture>
+              <p class="targeting-section__mob-text">В конце статьи</p>
+            </q-item>
+            <q-item class="targeting-section__mob-item">
+              <picture>
+                <img class="targeting-section__mob-img" src="~assets/main/middle-article.png" srcset="~assets/main/middle-article.png 1x, ~assets/main/middle-article-2x.png 2x" alt="middle article">
+              </picture>
+              <p class="targeting-section__mob-text">В середине статьи</p>
+            </q-item>
+            <q-item class="targeting-section__mob-item">
+              <picture>
+                <img class="targeting-section__mob-img" src="~assets/main/side-article.png" srcset="~assets/main/side-article.png 1x, ~assets/main/side-article-2x.png 2x" alt="side article">
+              </picture>
+              <p class="targeting-section__mob-text">На правой боковой панели</p>
+            </q-item>
+          </q-list>
+        </div>
+        <p class="targeting-section__intro intro intro--white intro--bold">Доступные настройки таргетов</p>
+        <q-list class="targeting-section__target-list">
+          <q-item class="targeting-section__target-item"><p>География</p></q-item>
+          <q-item class="targeting-section__target-item"><p>Устройства <br>(ПК/моб)</p></q-item>
+          <q-item class="targeting-section__target-item"><p>Операционные системы</p></q-item>
+          <q-item class="targeting-section__target-item"><p>Операторы связи</p></q-item>
+          <q-item class="targeting-section__target-item"><p>Браузеры</p></q-item>
+        </q-list>
+        <q-list class="targeting-section__target-list-big">
+          <q-item class="targeting-section__target-item-big">
+            <p>Интеграция с DMP- платформами</p>
+            <ul>
+              <li>AmberData</li>
+              <li>Weborama и другими</li>
+            </ul>
+          </q-item>
+          <q-item class="targeting-section__target-item-big">
+            <p>Аудиторные данные</p>
+            <ul>
+              <li>соцдем (пол/возраст)</li>
+              <li>интересы</li>
+              <li>Retargeting, LAL</li>
+            </ul>
+          </q-item>
+          <q-item class="targeting-section__target-item-big targeting-section__target-item-big--extra">
+            <p>Ключевые слова</p>
+            <span>
+              Покажем в статьях, где есть те или иные слова
+            </span>
+          </q-item>
+          <q-item class="targeting-section__target-item-big targeting-section__target-item-big--extra">
+            <p>Список площадок</p>
+            <span>
+              Подбор тематических площадок
+            </span>
+          </q-item>
+        </q-list>
+        <q-btn class="targeting-section__main-btn main-btn main-btn--blue">Разместить рекламу</q-btn>
+      </div>
+
     </section>
   </q-page>
 </template>
@@ -30,4 +102,6 @@
 <script setup lang="ts">
 import ImgSlider from 'components/ImgSlider.vue';
 import GeoSlider from 'components/GeoSlider.vue';
+import TargetingSlider from 'components/TargetingSlider.vue';
+import TargetingSliderMob from 'components/TargetingSliderMob.vue';
 </script>
